aad9f93baff4c81dd650c37318967e8fc8e62e2b
oncet <camilorivera86@gmail.com>
2020-12-05 01:28:31 -0300

    initial commit

---
+++ index.js
@@ -0,0 +1,17 @@
+(async function () {
+  console.log('hey there')
+  const { headers } = await fetch('https://api.github.com/repos/facebook/react/commits', { method: 'HEAD' })
+  const lastPageLink = [...headers.get('link').matchAll(/\W<(.+?)>; rel="last"/gm)].map(link => link[1])[0]
+  console.log(lastPageLink)
+  const response = await fetch(lastPageLink)
+  const commits = await response.json()
+  const oldest = commits
+    .sort((first, second) => (
+      new Date(second.commit.author.date) - new Date(first.commit.author.date)
+    )).pop()
+    
+  // console.log(commits.map(({ commit }) => commit.author.date));
+  console.log(oldest.html_url);
+
+  window.open(oldest.html_url)
+})();
