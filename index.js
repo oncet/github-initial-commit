17614cf7c2467ed5a2faeaa0dc912f7fd5eed657
Camilo <camilorivera86@gmail.com>
2020-12-17 21:53:15 -0300

    fix style

--- index.js
+++ index.js
@@ -6,7 +6,7 @@
           active: true,
           windowId: chrome.windows.WINDOW_ID_CURRENT,
         }, tabs => resolve(tabs[0]))
-      } catch(error) {
+      } catch (error) {
         reject(error)
       }
     })
@@ -20,7 +20,7 @@
   try {
     urlFragment = url.match(/github\.com\/([^\/]+\/[^\/]+)/)[1]
   } catch {
-    document.querySelector('h1').innerText = 'Not in a GitHub repo :('
+    document.querySelector('h1').innerText = 'Not in a GitHub repo?'
     return
   }
 
@@ -39,16 +39,16 @@
   // Extract last page link from headers
   const links = response.headers.get('link')?.matchAll(/\W<(.+?)>; rel="last"/gm)
 
-  const [ lastPageLink ] = links ? [...links].map(link => link[1]) : []
+  const [lastPageLink] = links ? [...links].map(link => link[1]) : []
 
   const lastPageResponse = lastPageLink && await fetch(lastPageLink)
-  
+
   const lastPageCommits = lastPageResponse ? await lastPageResponse.json() : await response.json()
 
   // Get oldest commit
   const oldest = lastPageCommits.sort((first, second) => (
-      new Date(second.commit.author.date) - new Date(first.commit.author.date)
-    )).pop()
+    new Date(second.commit.author.date) - new Date(first.commit.author.date)
+  )).pop()
 
   window.open(oldest.html_url)
 })()
